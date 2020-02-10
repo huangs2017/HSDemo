01500c4dd48a44452f9b5cf0c9aedaf7112bcb4f
huangsong <2455230388@qq.com>
2020-02-10 22:28:30 +0800

    - OKHttp

--- app/src/main/java/network/OkHttp/Callback.java
+++
@@ -1,9 +0,0 @@
-package network.OkHttp;
-
-import network.OkHttp.bean.Response;
-
-public interface Callback {
-    void onFailure(Throwable throwable);
-
-    void onResponse(Response response);
-}
