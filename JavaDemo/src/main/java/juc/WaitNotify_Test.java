f85045004468c388a221e850bb470f74385302e4
huangsong <2455230388@qq.com>
2020-02-18 20:54:33 +0800

    Initial commit

--- JavaDemo/src/main/java/juc/WaitNotify_Test.java
+++ JavaDemo/src/main/java/juc/WaitNotify_Test.java
@@ -8,7 +8,7 @@ import Util.Log;
     基于wait/notify的线程通信
     wait/notify必须与synchronized一同使用
     wait释放锁、notify不释放锁
-    notify只会通知一个wait中的线程，并把锁给他
+    notify只会通知一个wait中的线程，并把锁给它
  */
 public class WaitNotify_Test {
 
@@ -56,7 +56,7 @@ public class WaitNotify_Test {
         synchronized (lock) {
             try {
                 Log.i("线程" + Thread.currentThread().getName() + "业务处理，发现有需要的数据没准备好，则发起等待");
-                lock.wait(); //wait操作释放锁,否则其他线程无法进入put方法
+                lock.wait(); //wait操作释放锁，否则其他线程无法进入put方法
                 Log.i("线程" + Thread.currentThread().getName() + "被唤醒");
                 for (String s : list) {
                     Log.i("线程" + Thread.currentThread().getName() + "获取元素:" + s);
